57dede3f3f19e799ebf2f3139e7877914618070f
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-01-30 23:36:37 +0300

    Time: 146 ms (35.88%) | Memory: 31.9 MB (49.47%) - LeetSync

--- 2085-array-with-elements-not-equal-to-average-of-neighbors/array-with-elements-not-equal-to-average-of-neighbors.py
+++ 2085-array-with-elements-not-equal-to-average-of-neighbors/array-with-elements-not-equal-to-average-of-neighbors.py
@@ -4,14 +4,13 @@ class Solution:
         ans = []
         median = len(nums) // 2
         i = 0
-        while (i + median + 1) < len(nums):
-            ans.append(nums[i])
-            ans.append(nums[i + median +1])
-            i += 1
+        j = len(nums) -1
+        while i<j:
+          ans.append(nums[i])
+          ans.append(nums[j])
+          i += 1
+          j -= 1
         if len(nums) % 2 == 1:
-            ans.append(nums[median])
-        else:
             ans.append(nums[i])
-            ans.append(nums[median])
         return ans
         
