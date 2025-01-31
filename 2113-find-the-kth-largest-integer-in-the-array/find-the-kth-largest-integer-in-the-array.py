83b4aad04cbf4667a3c2b94d675515576ccf0e89
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-01-31 12:00:23 +0300

    Time: 27 ms (69.25%) | Memory: 24.5 MB (93.35%) - LeetSync

--- 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.py
+++ 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.py
@@ -2,7 +2,6 @@ class Solution:
     def kthLargestNumber(self, nums: List[str], k: int) -> str:
         if len(nums) == 0 or k > len(nums):
            return "" 
-        my_list = list(map(int, nums))
-        my_list.sort()
-        return str(my_list[-k])
+        nums.sort(key= lambda x: int(x))
+        return nums[-k]
         
