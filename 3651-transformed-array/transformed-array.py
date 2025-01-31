bdaa17d9c7616714160f55ae884242403581fad4
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-01-31 22:02:51 +0300

    Time: 55 ms (51.25%) | Memory: 18 MB (6.31%) - LeetSync

--- 3651-transformed-array/transformed-array.py
+++ 3651-transformed-array/transformed-array.py
@@ -3,15 +3,5 @@ class Solution:
 
         res = []
         n = len(nums)
-        for i, num in enumerate(nums):
-            # when numbers[i] == 0
-            if num == 0:
-                res.append(num)
-            # when numbers[i] > 0 and  when numbers[i] < 0
-            else:
-                print(num)
-                r = (num + i) % n
-                print(r)
-                res.append(nums[r])
-        return res
+        return [nums[i] if num == 0 else nums[(num + i) % n] for i, num in enumerate(nums)]
         
