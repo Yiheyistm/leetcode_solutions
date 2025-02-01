028ed513ab164002b1dda2eaae722277243ea5c0
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-02-02 00:07:56 +0300

    Time: 8 ms (28.18%) | Memory: 18 MB (16.86%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -11,14 +11,10 @@ class Solution:
         }
         res = 0
 
-        for i in range(len(s) - 1, -1, -1):
-            if symbol[s[i]] > symbol[s[i-1]] and i > 0:
-                res += symbol[s[i]] - symbol[s[i -1]]
-                continue
-            elif  i +1 < len(s) and symbol[s[i]] < symbol[s[i + 1]] :
-                continue
+        for i in range(len(s)):
+            if i + 1 < len(s) and symbol[s[i]] < symbol[s[i + 1]]:
+                res -= symbol[s[i]]
             else:
                 res += symbol[s[i]]
-            print(res)
         return res
              
