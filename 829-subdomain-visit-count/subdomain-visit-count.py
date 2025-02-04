855b50deaaa494e597fc0241e85a3523261336e9
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-02-04 14:46:37 +0300

    Time: 7 ms (46.10%) | Memory: 17.9 MB (40.34%) - LeetSync

--- 829-subdomain-visit-count/subdomain-visit-count.py
+++ 829-subdomain-visit-count/subdomain-visit-count.py
@@ -3,18 +3,14 @@ class Solution:
         visited_dict = {}
         for web in cpdomains: 
             no, link = web.split(' ')
-            i = 0
             for _ in range(link.count('.') + 1):
-                key = link
-                visited_dict[key] = visited_dict.get(key, 0) + int(no)
+                visited_dict[link] = visited_dict.get(link, 0) + int(no)
                 idx = link.find('.')
                 link = link[idx+1:]
                
         res = []
         for key, value in visited_dict.items():
             res.append(f"{value} {key}") 
-        
-        print(res)
         return res
 
 
