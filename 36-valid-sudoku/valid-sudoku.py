4dce5b378fe83552bb647e32f9801722fde8f91e
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-02-07 16:20:08 +0300

    Time: 2 ms (87.12%) | Memory: 17.7 MB (61.83%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,31 @@
+class Solution:
+    def checker(self,board, idx_lst, a, b) ->bool:
+        for idx in idx_lst:
+            i, j = idx
+            if i == a or j == b:
+                return False
+        tr_i, tr_j = a // 3, b // 3
+        print(tr_i, tr_j)
+        for i in range(tr_i * 3, tr_i * 3 + 3):
+            for j in range(tr_j * 3, tr_j * 3 + 3):
+                if board[i][j] == board[a][b] and a != i and b != j:
+                    return False
+        return True
+
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        sodoku_map = defaultdict(list)
+        for i in range(9):
+            for j in range(9):
+                cell = board[i][j]
+                if cell in sodoku_map:
+                    ckr = self.checker(board,sodoku_map[cell], i, j)
+                    if ckr:
+                        sodoku_map[cell].append([i,j])
+                    else:
+                        print(sodoku_map)
+                        return False
+                elif cell.isalnum():
+                    sodoku_map[cell].append([i,j])
+                else:
+                    continue
+        return True
