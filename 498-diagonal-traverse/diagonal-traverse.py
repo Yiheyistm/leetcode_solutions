ef0adcfb9a5308a6272aff1df86aa3adcf3c00ed
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-02-07 22:52:55 +0300

    Time: 23 ms (17.43%) | Memory: 19.7 MB (58.71%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.py
+++ 498-diagonal-traverse/diagonal-traverse.py
@@ -1,45 +1,35 @@
 class Solution:
-    def move_diagonal(self, mat, a ,b, dir):
+    def move_diagonal(self, mat, i ,j, dir):
         ls = []
-        i , j = a, b
-        if dir == 'bltr':
-            while 0 <= i < len(mat) and 0 <= j < len(mat[0]):
-                ls.append(mat[i][j])
+        while 0 <= i < len(mat) and 0 <= j < len(mat[0]):
+            ls.append(mat[i][j])
+            if dir == 'bltr':
                 i -= 1
                 j += 1
-        else:
-            while 0 <= i < len(mat) and 0 <= j < len(mat[0]):
-                ls.append(mat[i][j])
+            else:
                 i += 1
                 j -= 1
+               
         idx = (i+1, j) if dir == 'bltr' else (i, j+1)
         return ls, idx
 
 
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        n = len(mat)
-        m = len(mat[0])
         res = []
         i = j = 0
-        while i < n and j < m:
+        while i < len(mat) and j < len(mat[0]):
             if (i + j) % 2 == 0:
-                lst, idx = self.move_diagonal(mat,i,j,'bltr')
+                lst, (i, j) = self.move_diagonal(mat,i,j,'bltr')
                 res.extend(lst)
-                if idx[1] >= m:
-                    i = idx[0] + 1
-                    j = idx[1] - 1
-                else:
-                    i = idx[0]
-                    j = idx[1]
+                if j >= len(mat[0]):
+                    i +=  1
+                    j -=  1
             else:
-                lst, idx = self.move_diagonal(mat,i,j,'trbl')
+                lst, (i, j) = self.move_diagonal(mat,i,j,'trbl')
                 res.extend(lst)
-                if idx[0] >= n:
-                    i = idx[0]-1
-                    j = idx[1]+ 1
-                else:
-                    i = idx[0]
-                    j = idx[1]
+                if i >= len(mat):
+                    i -= 1
+                    j += 1
         return res
 
 
