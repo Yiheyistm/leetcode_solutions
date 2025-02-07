abb5dcd1d92606af013d7428c772998e6c56c515
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-02-07 13:00:19 +0300

    Time: 341 ms (72.77%) | Memory: 46.4 MB (66.67%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.py
+++ 1364-tuple-with-same-product/tuple-with-same-product.py
@@ -1,16 +1,15 @@
 class Solution:
     def tupleSameProduct(self, nums: List[int]) -> int:
-        my_map = defaultdict(list)
+        my_map = defaultdict(int)
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 prdt = nums[i] * nums[j]
-                my_map[prdt].append([i,j])
+                my_map[prdt] += 1
 
         no_tuples = 0
         for values in my_map.values():
-            n = len(values)
-            if n > 1:
-                no_tuples += (2*n) * (2*(n - 1))
+            if values > 1:
+                no_tuples += (2*values) * (2*(values - 1))
         return no_tuples
 
 
