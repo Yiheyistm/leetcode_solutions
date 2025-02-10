0871f07a9d88172b30a2ce3ff63d2814702f9009
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-02-10 09:52:24 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (36.87%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.py
+++ 861-flipping-an-image/flipping-an-image.py
@@ -6,23 +6,15 @@ class Solution:
                 sp = n - j -1
                 if j < sp:
                     image[i][j],image[i][sp] = image[i][sp],image[i][j]
-                    if image[i][j] == 0:
-                        image[i][j] = 1
-                    else:
-                        image[i][j] = 0
-
-                    if image[i][sp] == 0:
-                        image[i][sp] = 1
-                    else:
-                        image[i][sp] = 0
-                elif j == sp:
-                    if image[i][j] == 0:
-                        image[i][j] = 1
-                    else:
-                        image[i][j] = 0
-
                 else:
                     break
+        for i in range(n):
+            for j in range(n):
+                if image[i][j] == 0:
+                        image[i][j] = 1
+                else:
+                    image[i][j] = 0
+
         return image
 
         
