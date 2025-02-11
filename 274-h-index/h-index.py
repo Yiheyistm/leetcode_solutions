acc5ea23512ba9291724bf35a53c9b1f7b56a471
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-02-11 16:06:54 +0300

    Time: 3 ms (27.41%) | Memory: 17.9 MB (91.89%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -1,11 +1,12 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        citations.sort(reverse = True)
+        citations.sort()
+        n = len(citations)
         max_h = 0
-        print(citations)
-        for i in range(len(citations)):
-            if citations[i] >= i + 1:
-                max_h = i + 1
+        for i in range(n):
+            curr = n - i
+            if citations[i] >= curr:
+                max_h = max(max_h, curr)
 
         return max_h
 
