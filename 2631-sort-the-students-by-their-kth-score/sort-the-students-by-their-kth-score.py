e1171590a8a6cbdbf49c33bad3787108c55462ee
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-02-13 22:32:49 +0300

    Time: 3 ms (43.32%) | Memory: 22.8 MB (13.87%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.py
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.py
@@ -1,5 +1,16 @@
 class Solution:
     def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
-        score.sort(key = lambda item: item[k], reverse = True)
+        matrix_map = defaultdict(list)
+        for i in range(len(score)):
+            matrix_map[score[i][k]] = score[i]
+
+        key_list = list(matrix_map.keys())
+        key_list.sort(reverse = True)
+        score = []
+        for k in key_list:
+            score.append(matrix_map[k])
+        del matrix_map
+        del key_list
         return score
+        
         
