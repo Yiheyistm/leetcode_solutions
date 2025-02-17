8f30acd1b9a4ce45828daca36e1e6d55b314218f
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-02-17 21:12:11 +0300

    Time: 68 ms (41.54%) | Memory: 18.1 MB (33.02%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.py
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.py
@@ -1,12 +1,12 @@
 class Solution:
     def minimumSteps(self, s: str) -> int:
-        s = list(s)
         steps = 0
-        freq_map = defaultdict(int)
+        zero_count = 0
         for r in range(len(s) - 1, -1, -1):
-            freq_map[s[r]] += 1
-            if s[r] == '1':
-                steps += freq_map['0']
+            if s[r] == '0':
+                zero_count += 1
+            elif s[r] == '1':
+                steps += zero_count
 
         return steps
 
