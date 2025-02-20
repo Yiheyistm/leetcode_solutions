ca86c43b1f163da4d93b6382e73aee5671a9fbd9
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-02-20 10:17:18 +0300

    Time: 103 ms (5.56%) | Memory: 18.7 MB (12.74%) - LeetSync

---
+++ 1184-car-pooling/car-pooling.py
@@ -0,0 +1,16 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        line_sweep = [0] * 10001
+        for passanger, start, end in trips:
+            line_sweep[start] += passanger
+            line_sweep[end] -= passanger
+        route = [0]
+        for i, line in enumerate(line_sweep, start = 1):
+            route.append(route[i - 1] + line)
+            print(route[i])
+            if route[i] > capacity:
+                return False
+        return True
+
+        
+        
