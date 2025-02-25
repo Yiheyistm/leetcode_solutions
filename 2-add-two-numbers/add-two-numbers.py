cae90e89c399743be939926dc83b7a8e7b176fca
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-02-26 00:14:29 +0300

    Time: 100 ms (5.84%) | Memory: 18.1 MB (14.70%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,40 +5,24 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        
-        res = ListNode()
-        curr = res
-        q = 0
-        while l1 and l2:
-            sm = l1.val + l2.val + q
-            if sm >= 10:
-                q, r = divmod(sm, 10)
-                node = ListNode(r)
-            else:
-                node = ListNode(sm)
-                q = 0
-            curr.next = node
-            curr = node
-            l1 = l1.next
-            l2 = l2.next
+        def calc(l1, l2, q): 
+            if not l1 and  not l2 and  not q:
+                return None
+            val_1 = l1.val if l1 else 0
+            val_2 = l2.val if l2 else 0
 
-        if l1 or l2:
-            trav = l1 or l2
-            while trav:
-                sm = trav.val + q
-                if sm >= 10:
-                    q, r = divmod(sm, 10)
-                    node = ListNode(r)
-                else:
-                    node = ListNode(sm)
-                    q = 0
-                trav = trav.next
-                curr.next = node
-                curr = node
-        if q:
-            curr.next = ListNode(q)
-            curr = curr.next
-        return res.next
+            sm = val_1 + val_2 + q
+            q, r = divmod(sm, 10)
+            node = ListNode(r)
 
+            l1_next = l1.next if l1 else None
+            l2_next = l2.next if l2 else None
+            print(l1_next, l2_next ,q)
+            node.next = calc(l1_next, l2_next, q)
+
+            return node
+
+
+        return calc(l1, l2, 0)
 
         
