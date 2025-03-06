2d7804cfd33106c1bf4847d485158816e36eb853
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-03-06 15:32:33 +0300

    Time: 187 ms (88.21%) | Memory: 26.3 MB (60.56%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,26 +1,29 @@
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        maxStk = deque()
-        minStk = deque()
+        # Using monotonic queue we can track the largest and the smallest in the window
+        maxStk = deque() # for decreasing stack the largest is on index 0 (monotonic decreasing)
+        minStk = deque() # for increasing stack the the smallest is on index 0 (monotonic increasing)
         longest = 0
         l = 0
         for r, n in enumerate(nums):
-            while maxStk and maxStk[-1][0] < n:
+            # remove element on the decreasing stack until the largest element left which is greater than current
+            while maxStk and maxStk[-1] < n:
                 maxStk.pop()
-            maxStk.append((n, r))
-            while minStk and minStk[-1][0] > n:
-                minStk.pop()
-            minStk.append((n, r))
-            while maxStk[0][0] - minStk[0][0] > limit:
-                if maxStk[0][1] < minStk[0][1]:
-                    l = maxStk[0][1] + 1
-                    maxStk.popleft()
-                else:
-                    l = minStk[0][1] + 1
-                    minStk.popleft()
+            maxStk.append(n)
 
+            # remove element on the increasing stack until the smalles element left which is less than current
+            while minStk and minStk[-1] > n:
+                minStk.pop()
+            minStk.append(n)
+
+            # shrink window if max - min exceeds limit
+            while maxStk[0] - minStk[0] > limit:
+                if maxStk[0] == nums[l]:
+                    maxStk.popleft()
+                if minStk[0] == nums[l]:
+                    minStk.popleft()
+                l += 1
             longest = max(longest, r - l + 1)
-            
         return longest
 
 
