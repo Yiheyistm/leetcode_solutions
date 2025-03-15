8dbc0262a5d5b695b205c0452bfdacb7c027aee3
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-03-15 22:15:13 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (73.91%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -1,27 +1,7 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        memo = defaultdict(int)
-        memo2 = defaultdict(int)
+        # Five Evens digits which is 0 2 4 6 8
+        # Four Prime digits 2 3 5 7
         MOD = 10**9 + 7
-        def calc5(n):
-            if n == 0:
-                return 1
-            if n == 1:
-                return 5
-            ev1 = memo.get(n // 2) if  n // 2 in memo else calc5(n // 2)
-            memo[n // 2] = ev1
-            ev2 = memo.get(n - n // 2) if  n - n // 2 in memo else calc5(n - n // 2)
-            memo[n - n // 2] = ev2
-            return (ev1 * ev2) % MOD
-        
-        def calc4(n):
-            if n == 0:
-                return 1
-            if n == 1:
-                return 4
-            ev1 = memo2.get(n // 2) if  n // 2 in memo2 else calc4(n // 2)
-            memo2[n // 2] = ev1
-            ev2 = memo2.get(n - n // 2) if  n - n // 2 in memo2 else calc4(n - n // 2)
-            memo2[n - n // 2] = ev2
-            return (ev1 * ev2) % MOD
-        return (calc5(n - n//2) * calc4(n//2)) % MOD
+        return (pow(5, n - n//2, MOD) * pow(4 , n//2, MOD)) % MOD
+        # ((5 ** (n - n//2)%MOD) * (4 ** (n//2)) % MOD) % MOD
