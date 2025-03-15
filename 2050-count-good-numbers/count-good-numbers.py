329d2efcb7fe238027200d5508cd9a92d50bed33
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-03-15 22:23:46 +0300

    Time: 11 ms (0.92%) | Memory: 18.8 MB (5.96%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -1,7 +1,19 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        # Five Evens digits which is 0 2 4 6 8
-        # Four Prime digits 2 3 5 7
+        memo = defaultdict(int)
         MOD = 10**9 + 7
-        return (pow(5, n - n//2, MOD) * pow(4 , n//2, MOD)) % MOD
-        # ((5 ** (n - n//2)%MOD) * (4 ** (n//2)) % MOD) % MOD
+        def calc(x, n):
+            if n == 0:
+                return 1
+            if n == 1:
+                return x
+            ev1 = memo.get(n // 2) if  n // 2 in memo else calc(x, n // 2)
+            memo[n // 2] = ev1
+            ev2 = memo.get(n - n // 2) if  n - n // 2 in memo else calc(x, n - n // 2)
+            memo[n - n // 2] = ev2
+            return (ev1 * ev2) % MOD
+
+        even = calc(5, n - n//2)
+        memo = defaultdict(int) # reset the dictionary
+        odd = calc(4, n //2)
+        return (even * odd) % MOD
