5a9fb9fdb1a2ed563da832d462b67438c8851530
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-03-19 23:13:53 +0300

    Time: 19 ms (99.45%) | Memory: 57 MB (98.29%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -1,18 +1,4 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        global_store = []
-        def backtrack(idx, store):
-            nonlocal n, k, global_store
-            if len(store) == k:
-                # print('YES')
-                global_store.append(store[:])
-                return
-
-            for j in range(idx, n +1):
-                store.append(j)
-                backtrack(j+1, store)
-                store.pop()
-        backtrack(1, [])
-        return global_store
-
+        return list(combinations(range(1, n+1), k))
         
