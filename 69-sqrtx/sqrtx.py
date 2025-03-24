346d492cb31576de8ef3c6000cfd7aea26966238
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-03-24 19:39:12 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (81.27%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -3,11 +3,9 @@ class Solution:
         l, r = 0, x
         while l <= r:
             mid = (r + l) // 2
-            if mid * mid == x:
-                return mid
-            elif mid * mid < x:
+            if mid * mid <= x:
                 l = mid + 1
             else:
                 r = mid -1
-        return l if l ** 2 == x else l - 1
+        return r
         
