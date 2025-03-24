d812ddfdc8bee7cf318946c49d17018f3c903486
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-03-24 15:35:37 +0300

    Time: 408 ms (5.19%) | Memory: 29.5 MB (5.68%) - LeetSync

--- 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.py
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.py
@@ -1,18 +1,25 @@
 class Solution:
     def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
-        min_pos = max_pos = left = -1 
-        res = 0  
-
-        for i, num in enumerate(nums):
-            if num < minK or num > maxK:
-                left = i  
-                min_pos = max_pos = -1
-            
-            if num == minK:
-                min_pos = i
-            if num == maxK:
-                max_pos = i
-            
-            if min_pos != -1 and max_pos != -1:
-                res += max(0, min(min_pos, max_pos) - left)
+        dec = deque()
+        inc = deque()
+        left = 0
+        invalid = -1
+        res = 0
+        for i in range(len(nums)):
+            while inc and nums[inc[-1]] >= nums[i]:
+                inc.pop()
+                
+            while dec and nums[dec[-1]] <= nums[i]:
+                dec.pop()
+                
+            inc.append(i)
+            dec.append(i)
+            while dec and inc and (nums[dec[0]] > maxK or nums[inc[0]] < minK):
+                invalid = i
+                num = nums[left]
+                if nums[dec[0]] == num: dec.popleft()
+                if nums[inc[0]] == num: inc.popleft()
+                left += 1
+            if dec and  inc and nums[dec[0]] == maxK and nums[inc[0]] == minK:
+                res += max(0, min(dec[0], inc[0]) - invalid)
         return res
