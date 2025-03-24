0312a8d31dd8ab64a30793aedde656874eb3af3c
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-03-24 22:11:45 +0300

    Time: 0 ms (100.00%) | Memory: 18.6 MB (94.31%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,18 +1,10 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        def rec(l, r):
-            nonlocal target, nums
-            mid = (l + r) // 2
-            if nums[mid] == target:
-                return mid
-            if l >= r:
-                return -1
-            if target > nums[mid]:
-               return rec(mid + 1, r)
-            else: 
-                return rec(l, mid - 1)
-                
-        return rec(0, len(nums) -1)
+        idx = bisect_left(nums, target)
+        if idx == len(nums) or nums[idx] != target:
+            return -1
+        return idx
+
         
         
 
