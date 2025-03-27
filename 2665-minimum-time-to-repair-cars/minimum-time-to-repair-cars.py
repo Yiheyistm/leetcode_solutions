8b575e399460be2b33fa5837c38274bcdefe98fe
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-03-28 00:52:56 +0300

    Time: 963 ms (34.56%) | Memory: 22 MB (20.60%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
@@ -1,26 +1,20 @@
 class Solution:
     def repairCars(self, ranks: List[int], cars: int) -> int:
-        '''
-        mn_time = the required time
-        mx = find the the optimal max time that the best mechanics spent to repair all cars by his own
-        so it is obvious that mn_time <= mx  if his fellow mechanis help him
-        we can find the mid time and calc the no of cars that cleaned at that time
-        if numbers of cars less than the total car implies that all cars are not repair in the expexted mid time else numbers of cars greater or equal to total car means they can repair all cars below or at mid times
-        '''
-        max_time = min(ranks) * cars ** 2 # the optimal max time 
-        min_time = 1
-        while min_time < max_time:
-            # r* n**2 = m
-            # n = math.sqrt(m/r)
-            mid = (max_time + min_time) // 2 # does all cars are repair in the mid time 
-            total_cars = 0 # count the no cars do repair in the mid time
-            for r in ranks: # check does mechanics finish repair all cars
-                total_cars += int(math.sqrt(mid / r))
-                if total_cars >= cars: break
-            if total_cars >= cars:
+        def can_repair(mid_time):
+            t_car = 0
+            for r in ranks:
+              t_car += int(math.sqrt(mid_time / r)) 
+            return t_car >= cars
+
+        max_time = min(ranks) * cars ** 2
+        min_time = 0
+        while min_time + 1 < max_time:
+            mid = (max_time + min_time) // 2 
+            if can_repair(mid):
                 max_time = mid
-            else: min_time = mid + 1
-        return min_time
+            else: min_time = mid
+        
+        return max_time
 
 
 
