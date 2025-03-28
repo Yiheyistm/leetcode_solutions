0355a35d9a104f7726c8462f433ee6dd0423eea3
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-03-28 21:57:56 +0300

    Time: 753 ms (20.54%) | Memory: 25.3 MB (47.73%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,15 +1,27 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        min_num = abs(min(nums))
-        max_num = max(nums)
-        count = [0] * (max_num + min_num + 1)
-        for num in nums:
-            count[num + min_num] += 1
+        def merge(l_arr, r_arr):
+            l, r = 0, 0
+            merged = []
+            while l < len(l_arr) and r < len(r_arr):
+                if l_arr and r_arr and l_arr[l] <= r_arr[r]:
+                    merged.append(l_arr[l])
+                    l += 1
+                elif l_arr and r_arr and l_arr[l] > r_arr[r]:
+                    merged.append(r_arr[r])
+                    r += 1
+            while l < len(l_arr):
+                merged.append(l_arr[l])
+                l += 1
+            while r < len(r_arr):
+                merged.append(r_arr[r])
+                r += 1
+            return merged
 
-        target = 0
-        for i, val in enumerate(count):
-            for j in range(val):
-                nums[target] = i - abs(min_num)
-                target += 1
-        return nums
+        def merge_sort(left, right, arr):
+            if right == left:
+                return [arr[left]]
+            mid = (left + right) // 2
+            return merge(merge_sort(left, mid, arr), merge_sort(mid + 1, right, arr))
+        return merge_sort(0, len(nums) - 1, nums)
         
