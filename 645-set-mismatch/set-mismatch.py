81cfbd9b43984fda01257ee8587715fbf7962f45
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-04-03 22:15:51 +0300

    Time: 23 ms (29.68%) | Memory: 19.2 MB (50.63%) - LeetSync

--- 645-set-mismatch/set-mismatch.py
+++ 645-set-mismatch/set-mismatch.py
@@ -1,13 +1,12 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         i = 0
-        res = OrderedDict()
+        res = {}
         n = len(nums)
         while i < n:
             correct_pos = nums[i] -1
             if nums[i] in res and nums[correct_pos] != nums[i]:
                 res.pop(nums[i])
-                res[i + 1] = 1
             if correct_pos == i:
                 i += 1
             else:
