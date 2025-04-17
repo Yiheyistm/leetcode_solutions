12829468934f2964aff78bf255c85080b283b2fc
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-04-18 00:51:23 +0300

    Time: 152 ms (77.64%) | Memory: 18.1 MB (91.82%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -5,7 +5,7 @@ class Solution:
             return -1
 
         n = len(grid)
-        visited = set((0 ,0))
+        visited = [[False] * n for _ in range(n)]
         path = deque([(0, 0, 1)])
         while path:
             i, j, p = path.popleft()
@@ -13,9 +13,9 @@ class Solution:
                 nr = i + dr; nc = j + dc
                 if  i == n - 1 and j == n -1:
                     return p
-                elif 0 <= nr < n and 0 <= nc < n and grid[nr][nc] == 0 and (nr, nc) not in visited:
+                elif 0 <= nr < n and 0 <= nc < n and grid[nr][nc] == 0 and not visited[nr][nc]:
                     path.append((nr, nc, p +  1))
-                    visited.add((nr, nc))
+                    visited[nr][nc] = True
 
         return -1
 
