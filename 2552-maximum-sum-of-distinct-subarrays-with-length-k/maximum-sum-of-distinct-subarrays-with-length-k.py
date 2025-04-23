cde4110dc79cbf0d11aea6d850b89ff749f265ae
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-04-23 09:51:37 +0300

    Time: 187 ms (9.53%) | Memory: 37.3 MB (46.77%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,15 +1,19 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
         r_sum = sum(nums[:k])
-        d = Counter(nums[:k])
-        max_sum = r_sum if len(d) == k else 0
+        freq = Counter(nums[:k])
+        max_sum = r_sum if len(freq) == k else 0
+        len_sub = len(freq)
         for i in range(k, len(nums)):
-            d[nums[i-k]] -= 1
-            if not d[nums[i-k]]: del d[nums[i-k]]
-            r_sum += nums[i] - nums[i - k]
-            d[nums[i]] += 1
+            freq[nums[i-k]] -= 1
+            if not freq[nums[i-k]]: 
+                len_sub -= 1
+                del freq[nums[i-k]]
 
-            if len(d) == k and max_sum < r_sum:
+            len_sub += 1 if nums[i] not in freq else 0
+            r_sum += nums[i] - nums[i - k]
+            freq[nums[i]] += 1
+            if len_sub == k and max_sum < r_sum:
                 max_sum = r_sum
 
         return max_sum
