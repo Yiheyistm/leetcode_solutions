ab85082ba72f09e51b95639ee245c1af36e2b824
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-04-28 01:38:50 +0300

    Time: 31 ms (27.70%) | Memory: 18.2 MB (15.88%) - LeetSync

---
+++ 945-snakes-and-ladders/snakes-and-ladders.py
@@ -0,0 +1,31 @@
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        def findRowCol(cell):
+            cell -= 1
+            r = (n - 1) - (cell // n)
+            if (n - 1 - r) % 2 == 0:
+                c = cell % n
+            else:
+                c = (n - 1) - (cell % n)
+            return r, c
+
+
+        n = len(board)
+        start = deque([(1, 0)])
+        visited = set([1])
+        while start:
+            curr, min_move = start.popleft()
+            if curr == n ** 2:
+                return min_move
+            for nxt in range(curr + 1, min(curr + 6, n ** 2) + 1):
+                r, c = findRowCol(nxt)
+                dest = board[r][c] if board[r][c] != -1 else nxt
+                if dest not in visited:
+                    visited.add(dest) 
+                    start.append((dest, min_move + 1))
+        
+        return -1
+
+
+
+        
