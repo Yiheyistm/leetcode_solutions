b921fcddd553f2f15734479876897e67908531b9
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-05-02 00:22:14 +0300

    Time: 1 ms (68.27%) | Memory: 18.2 MB (77.97%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,22 +1,28 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        def dfs(ch, p, visited):
-            visited.add(ch)
-            for neigh in graph[ch]:
-                if neigh not in visited:
-                    if dfs(neigh,ch, visited):
-                        return True
-                elif neigh != p:
-                    return True
-            return False
+        def find(x):
+            while x != parent[x]:
+                x = parent[parent[x]]
+            return x
+        def union(x,y):
+            parX = find(x)
+            parY = find(y)
+            if parX != parY:
+                if size[parX] >= size[parY]:
+                    parent[parY] = parX
+                    size[parX] += size[parY]
+                else:
+                    parent[parX] = parY
+                    size[parY] += size[parX]
 
-        graph = defaultdict(list)
+        n = len(edges)
+        parent = {i:i for i in range(1, n + 1)}
+        size = [1] * (n + 1)
+
         for a, b in edges:
-            visited = set()
-            graph[a].append(b)
-            graph[b].append(a)
-            if dfs(a, None, visited):
+            if find(a) == find(b):
                 return [a, b]
+            union(a,b)
 
 
 
