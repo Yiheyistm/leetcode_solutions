1dbe5f3f220822a14fe4e519208323d54340d7e0
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-05-16 18:59:10 +0300

    Time: 263 ms (59.23%) | Memory: 20.2 MB (59.62%) - LeetSync

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.py
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.py
@@ -1,26 +1,18 @@
 class Solution:
     def findTheLongestSubstring(self, s: str) -> int:
-        alp = {'a', 'e', 'i', 'o', 'u'}
-        xor_pfx = [0]
-        for ch in s:
-            if ch in alp:
-                xor_pfx.append(xor_pfx[-1] ^ ord(ch))
-            else:
-                xor_pfx.append(xor_pfx[-1])
-        pairs = [[xor_pfx[i], i] for i in range(len(xor_pfx))] 
-        
-        pairs.sort()
-        max_ = 0
-        l = 0
-        diff = False
-        for r in range(1, len(pairs)):
-            diff = False
-            if pairs[r][0] ^ pairs[l][0] != 0:
-                max_ = max(max_, pairs[r - 1][1] - pairs[l][1])
-                diff = True
-                l = r
-            
-        if not diff:
-            max_ = max(max_, pairs[-1][1] - pairs[l][1])
+        alpha = {'a':1, 'e': 2, 'i': 4, 'o': 8, 'u': 16}
+        dict_ = defaultdict(int)
+        dict_[0] = -1
+        temp = 0
+        max_diff = 0
+        for i, ch in enumerate(s):
+            if ch in 'aeiou':
+                temp ^= alpha[ch]
 
-        return max_ if xor_pfx[-1] else len(s)
+            if temp in dict_:
+                distance = i - dict_[temp]
+                max_diff = max(max_diff, distance)
+            else:
+                dict_[temp] = i
+        return max_diff
+
