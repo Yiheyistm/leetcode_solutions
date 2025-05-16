3c611c74f3f45478454e30653c12461ea81a9c8e
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-05-16 15:08:26 +0300

    Time: 7 ms (5.65%) | Memory: 17.8 MB (73.38%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -4,26 +4,11 @@ class Solution:
         i,j = len(a)-1, len(b) -1
         carry = 0
         while i >= 0 or j >= 0 or carry:
-            bit1 = a[i] if i >= 0 else '0'
-            bit2 = b[j] if j >= 0 else '0'
-            if bit1 == '1' and bit2 == '1':
-                if carry:
-                    res = '1' + res
-                else:
-                    res = '0' + res
-                    carry = 1
-            elif bit1 == '1' or bit2 == '1':
-                if carry:
-                    res = '0' + res
-                    carry = 1
-                else:
-                    res = '1' + res
-            else:
-                if carry:
-                    res = '1' + res
-                    carry = 0
-                else:
-                    res = '0' + res
+            bit1 = int(a[i]) if i >= 0 else 0
+            bit2 = int(b[j]) if j >= 0 else 0
+            val, rem = divmod(bit1 + bit2 + carry, 2)
+            res = str(rem) + res
+            carry = val
             i -= 1
             j -= 1
         return res
