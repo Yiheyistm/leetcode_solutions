00ce4d749b91932c727c20e64ef5ae164e5dd406
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-05-16 14:59:50 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (91.82%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,4 +1,31 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        return bin(int(a, 2) + int(b, 2))[2:]
+        res = ""
+        i,j = len(a)-1, len(b) -1
+        carry = 0
+        while i >= 0 or j >= 0 or carry:
+            bit1 = a[i] if i >= 0 else '0'
+            bit2 = b[j] if j >= 0 else '0'
+            if bit1 == '1' and bit2 == '1':
+                if carry:
+                    res = '1' + res
+                else:
+                    res = '0' + res
+                    carry = 1
+            elif bit1 == '1' or bit2 == '1':
+                if carry:
+                    res = '0' + res
+                    carry = 1
+                else:
+                    res = '1' + res
+            else:
+                if carry:
+                    res = '1' + res
+                    carry = 0
+                else:
+                    res = '0' + res
+            i -= 1
+            j -= 1
+        return res
+            
         
