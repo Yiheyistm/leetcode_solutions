c889f187a3410f38dbf26d5bdc1289e2cc5b8451
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-05-17 22:51:25 +0300

    Time: 10 ms (56.90%) | Memory: 17.7 MB (58.81%) - LeetSync

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.py
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.py
@@ -1,17 +1,13 @@
 class Solution:
     def countTriplets(self, arr: List[int]) -> int:
-        xor_pfx = [0]
-        for num in arr:
-            xor_pfx.append(xor_pfx[-1] ^ num)
-        
-        subarray = []
+        freq = { 0: -1}      
+        cnt = 0
         for i in range(len(arr)):
+            xor = 0
             for j in range(i + 1, len(arr)):
-                subarray.append((i, j))
+                xor ^= arr[j]
+                if xor == arr[i]:
+                    cnt += j - i
+        return cnt
         
-        ans = 0
-        for i, k in subarray:
-            if xor_pfx[k + 1] ^ xor_pfx[i] == 0:
-                ans += k - i
-        return ans
         
