67962455530975e4d1a7ed74a8ba1b1d242b0d26
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-05-19 22:23:22 +0300

    Time: 7 ms (91.84%) | Memory: 18.1 MB (23.13%) - LeetSync

--- 3422-find-the-n-th-value-after-k-seconds/find-the-n-th-value-after-k-seconds.py
+++ 3422-find-the-n-th-value-after-k-seconds/find-the-n-th-value-after-k-seconds.py
@@ -1,9 +1,5 @@
 class Solution:
     def valueAfterKSeconds(self, n: int, k: int) -> int:
-        pfx = [1] * n
-        for i in range(k):
-            for j in range(1, n):
-                pfx[j] += pfx[j -1]
-        return pfx[-1] % (10**9 + 7)
+        return math.comb(n + k - 1, k) % (10**9 + 7) # Combination with repeatition
 
         
