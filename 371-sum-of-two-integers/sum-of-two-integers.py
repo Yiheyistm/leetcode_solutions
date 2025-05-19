2068f138115fff3c31606082d9aeb35c41cfa695
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-05-19 21:32:39 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (47.81%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.py
+++ 371-sum-of-two-integers/sum-of-two-integers.py
@@ -1,25 +1,19 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
         if a < 0:
-            a = (1 << 32) + a
+            a = a + (1 << 32)
         if b < 0:
-            b = (1 << 32) + b
-        res = 0
-        carry = 0
-        k = 0
-        while a > 0 or b > 0 or carry and k < 32:
-            bit1 = 1 if a & 1 else 0
-            bit2 = 1 if b & 1 else 0
-            val, rem = divmod(bit1 + bit2 + carry, 2)
-            res = (rem << k) | res
-            carry = val
-            a >>= 1
-            b >>= 1
-            k += 1
+            b = b + (1 << 32)
 
-        if res.bit_length() >= 32: # if the res is negative
-            res -= 1 << res.bit_length()
+        while b != 0:
+            carry = a & b
+            a = a ^ b
+            b = carry << 1
+            b &= 0xFFFFFFFF
 
-        return res
+        if a >= (1 << 31):
+            a -= (1 << 32)
+
+        return a
         
         
