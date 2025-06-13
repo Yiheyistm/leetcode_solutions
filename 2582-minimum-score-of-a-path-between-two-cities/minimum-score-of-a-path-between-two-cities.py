c5d1dcfec2730a960aa9cd298f4ba7091baa2ae5
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-06-13 17:15:07 +0300

    Time: 426 ms (20.99%) | Memory: 64.2 MB (81.16%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
@@ -1,34 +1,27 @@
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
-        def find(x):
-            while x != parent[x]:
-                parent[x] = parent[parent[x]]
-                x = parent[x]
-            return parent[x]
-
-        def union(x, y, dis):
-            parX = find(x)
-            parY = find(y)
-            if parX != parY:
-                if size[parX] >= size[parY]:
-                    parent[parY] = parX
-                    size[parX] += size[parY]
-                    min_score[parX] = min(min_score[parX], min_score[parY], dis)
-                else:
-                    parent[parX] = parY
-                    size[parY] += size[parX]
-                    min_score[parY] = min(min_score[parX], min_score[parY], dis)
-            else:
-                min_score[parY] = min(dis, min_score[parY])
 
 
-        parent = [i for i in range(n + 1)]
-        size = [1 for _ in range(n + 1)]
-        min_score = [float('inf') for _ in range(n + 1)]
+
+        graph = defaultdict(list)
+        min_scores = [float('inf') for _ in range(n + 1)]
         for a, b, d  in roads:
-            min_score[a] = min(d,min_score[a])
-            min_score[b] = min(d,min_score[b])
-            union(a, b, d)
+            min_scores[a] = min(d,min_scores[a])
+            min_scores[b] = min(d,min_scores[b])
+            graph[a].append(b)
+            graph[b].append(a)
 
-        return min_score[find(1)]
+        min_score = float('inf')
+        visit = set()
+        path = [1]
+        while path:
+            p = path.pop()
+            min_score = min(min_score, min_scores[p])
+            visit.add(p)
+            for neigh in graph[p]:
+                if neigh not in visit:
+                    path.append(neigh)
+        
+
+        return min_score
         
