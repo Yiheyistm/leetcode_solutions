a60bec816c31ce80ed6eaca086ec8c23434d4d71
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-06-19 22:03:01 +0300

    Time: 0 ms (100.00%) | Memory: 45.4 MB (79.97%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.py
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.py
@@ -1,11 +1,5 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        indegree = defaultdict(int)
-        for a, b in edges:
-            indegree[a] += 1
-            indegree[b] += 1
-            if indegree[a] > 1:
-                return a
-            if indegree[b] > 1:
-                return b
+        return edges[0][0] if edges[1][0] == edges[0][0] or edges[1][1] == edges[0][0] else edges[0][1]
+        
         
