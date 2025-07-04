cda805351290651781b4a08c6c700de5028cf13e
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-07-04 23:33:09 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (91.65%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,30 +1,17 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        numToalph = {
-            2: "abc",
-            3: "def",
-            4: "ghi",
-            5: "jkl",
-            6: "mno",
-            7: "pqrs",
-            8: "tuv",
-            9: "wxyz"
+
+        if not digits:
+            return []
+
+        phone_map = {
+            '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+            '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'
         }
-
-        ans = []
-        n = len(digits)
-        def backtrack(i, stack):
-            if len(stack) == n and n >= 1:
-                ans.append(''.join(stack))
-                return
-            
-            for j in range(i, n):
-                for ch in numToalph[int(digits[j])]:
-                    stack.append(ch)
-                    backtrack(j + 1, stack)
-                    stack.pop()
-
-        backtrack(0,[])
-        return ans
+        groups = [phone_map[d] for d in digits]
+        
+        combos = product(*groups)
+        
+        return [''.join(letters) for letters in combos]
 
         
