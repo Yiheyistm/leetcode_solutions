141c407ff5d43c60c4093567d85dd3f79ffea793
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-07-10 18:21:33 +0300

    Time: 36 ms (76.03%) | Memory: 17.9 MB (6.52%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.py
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.py
@@ -4,12 +4,11 @@ class Solution:
         dup = 0
         for i in range(1, len(word)):
             if word[i] != word[i - 1]:
-                print(i)
                 cnt += dup
                 dup = 0
             else:
                 dup += 1
-                
+
         if dup:
             cnt += dup
         return cnt
