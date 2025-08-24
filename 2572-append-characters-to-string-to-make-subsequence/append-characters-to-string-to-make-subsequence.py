092e63ad12f4f7a8a78cd6cf5ea3488b57438afe
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-08-24 19:18:25 +0300

    Time: 15 ms (94.02%) | Memory: 19.9 MB (8.13%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.py
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.py
@@ -1,24 +1,10 @@
 class Solution:
     def appendCharacters(self, s: str, t: str) -> int:
-        waiting = defaultdict(deque)
-        waiting[t[0]].append(iter(t[1:]))
-        track = 0
+        dq = deque(list(t))
         for ch in s:
-            queue = waiting[ch]
-            waiting[ch] = deque()
-            if queue:
-                it = queue.popleft()
-                nxt = next(it, None)
-                if nxt:
-                    waiting[nxt].append(it)
-                    track += 1
-        
-        queue = waiting[t[track]]
-        if queue:
-            it = queue.pop()
-            return len(list(it)) + 1
-
-        return 0
+            if dq and ch == dq[0]:
+                dq.popleft()
+        return len(dq)
      
 
 
