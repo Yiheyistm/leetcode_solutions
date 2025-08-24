f09180089af9558a9a8692dabaa6863a73f9fb57
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-08-24 17:02:17 +0300

    Time: 266 ms (60.38%) | Memory: 19.6 MB (63.99%) - LeetSync

--- 808-number-of-matching-subsequences/number-of-matching-subsequences.py
+++ 808-number-of-matching-subsequences/number-of-matching-subsequences.py
@@ -1,36 +1,22 @@
+
 class Solution:
-    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
-        def bs(ch, max_):
-            if ch not in char_dict:
-                return -1
-
-            l = -1
-            h = len(char_dict[ch]) - 1
-            while l + 1 < h:
-                mid = (l + h) // 2
-                if char_dict[ch][mid] >= max_: h = mid
-                else: l = mid
-            return char_dict[ch][h]
-
-        cnt = 0
-        char_dict = defaultdict(list)
-        for i, ch in enumerate(s):
-            char_dict[ch].append(i)
-
+    def numMatchingSubseq(self, s: str, words: list[str]) -> int:
+        waiting = defaultdict(deque)
         for word in words:
-            max_ = 0
-            ln = 0
-            for ch in word:
-                md = bs(ch, max_)
-                if md < max_: break
-                max_ = md + 1
-                ln += 1
-
-            if ln == len(word):
-                cnt += 1
-        return cnt
-            
-
+            waiting[word[0]].append(iter(word[1:]))
         
-
+        count = 0
         
+        for ch in s:
+            queue = waiting[ch]
+            waiting[ch] = deque()
+
+            while queue:
+                it = queue.popleft()
+                nxt = next(it, None)
+                if nxt:
+                    waiting[nxt].append(it)
+                else:
+                    count += 1
+        
+        return count
