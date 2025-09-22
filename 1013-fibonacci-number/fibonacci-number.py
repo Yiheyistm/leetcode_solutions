02735ef71a0090d04c05abaa7164095558de95e5
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-09-22 11:12:28 +0300

    Time: 39 ms (63.62%) | Memory: 17.6 MB (89.16%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -4,7 +4,7 @@ class Solution:
         def dp(n):
             if n == 1 or n == 0: return n
             if memo[n] == -1:
-                memo[n] = self.fib(n -1) + self.fib(n - 2)
+                memo[n] = dp(n -1) + dp(n - 2)
             return memo[n]
         return dp(n)
         
