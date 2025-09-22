b10d171dc874f278c073815254d55f3bc53bf4b9
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-09-22 14:54:59 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (33.98%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -3,11 +3,14 @@ class Solution:
         n = len(nums)
         memo = [-1] * (n + 1)
         def dp(h):
-            if h >= n: return 0
+            if h == 0: return nums[h]
+            if h == 1:
+                return max(nums[0],nums[1])
 
             if memo[h] == -1:
-                memo[h] = nums[h] + max(dp(h + 2), dp(h + 3))
+                memo[h] = max(dp(h - 2) + nums[h], dp(h - 1))
             return memo[h]
-        return max(dp(0), dp(1))
+
+        return dp(n - 1)
 
         
