1bb189ce62748f966aa1259395f8d42ed508a466
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-09-22 12:04:39 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (78.34%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,13 +1,9 @@
 class Solution:
+    def __init__(self):
+        self.memo = [-1] * 46
+
     def climbStairs(self, n: int) -> int:
-        memo = [-1] * (n + 1)
-
-        def dp(st):
-            if st == n:
-                return 1
-            elif st > n: return 0
-
-            if memo[st] == -1:
-                memo[st] = dp(st + 1) + dp(st + 2)
-            return memo[st]
-        return dp(0)
+        if n == 0:return 1
+        elif n <  0: return 0
+        if self.memo[n] == -1: self.memo[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        return self.memo[n]
