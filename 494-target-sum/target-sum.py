0e452e98bbf8c550e48e47564f37949903ea8d91
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-09-23 20:44:16 +0300

    Time: 115 ms (66.06%) | Memory: 22.8 MB (44.06%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,15 +1,17 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         N = len(nums)
-        store = [[-1] * 1001 for _ in range(N)]
+        max_sum = sum(nums)
+        off_set = max_sum
+        store = [[-1] * (2 * max_sum + 1) for _ in range(N)]
 
         def dfs(i, sm=0):
             if i == N:
                 if sm == target:
                     return 1
                 return 0
-            if store[i][sm] == -1:
-                store[i][sm] = dfs(i + 1, sm - nums[i]) + dfs(i + 1, sm + nums[i])
+            if store[i][sm + off_set] == -1:
+                store[i][sm + off_set] = dfs(i + 1, sm - nums[i]) + dfs(i + 1, sm + nums[i])
 
-            return store[i][sm]
+            return store[i][sm + off_set]
         return dfs(0 ,0)
