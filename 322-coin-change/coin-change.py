4e6e39a9bf27a4da69c59cad6e83b6fcd593809d
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-09-25 00:32:06 +0300

    Time: 723 ms (74.97%) | Memory: 18.3 MB (56.83%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,24 +1,13 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         n = len(coins)
-        store = [[-1] * (amount + 1) for _ in range(n)]
-        def dfs(i, rem):
-            if rem == 0:
-                return 0
-            if i == n or rem < 0:
-                return float("inf")
+        store = [float('inf')] * (amount + 1)
+        store[0] = 0
+        for coin in coins:
+            for amt in range(coin, amount +1):
+                store[amt] = min(store[amt], store[amt - coin] + 1)
 
-            if store[i][rem] == -1:
-                skip = dfs(i + 1, rem)
-                take = float('inf')
-                if rem >= coins[i]:
-                    take = (dfs(i, rem - coins[i]))
-                    if take != float('inf'):
-                        take += 1
-                store[i][rem]  = min(skip, take)
-            return store[i][rem]
-
-        res = dfs(0, amount)
+        res = store[amount]
         return res if res != float('inf') else -1
             
         
