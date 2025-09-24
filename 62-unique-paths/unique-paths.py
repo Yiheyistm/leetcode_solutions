88f855df20fb8ca1679d015d9ab9af4c3299313f
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-09-24 23:59:40 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (25.92%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,16 +1,12 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dir = [(0, 1), (1, 0)]
-        store = [[-1] * n for _ in range(m)]
-        def dfs(i, j):
-            if i >= m or j >= n:
-                return 0
-            if i == m -1 and j == n - 1:
-                return 1
-            if store[i][j] == -1:
-                cnt = 0
-                for (di, dj) in dir:
-                    cnt += dfs(di + i, dj + j)
-                store[i][j] = cnt
-            return store[i][j]
-        return dfs(0, 0)
+       
+        # Bottom Up DP with 1D array
+        dp = [0] * n
+        dp[0]= 1 
+        for i in range(m):
+            for j in range(n):
+                if j > 0:
+                    dp[j] +=dp[j -1]
+        return dp[-1]
+        
