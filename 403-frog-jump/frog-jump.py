efc279f28b206610249c12e17668712ad9af1bd4
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-09-26 00:47:09 +0300

    Time: 419 ms (20.18%) | Memory: 128.1 MB (5.22%) - LeetSync

--- 403-frog-jump/frog-jump.py
+++ 403-frog-jump/frog-jump.py
@@ -3,17 +3,19 @@ class Solution:
         pos = {num:i for i, num in enumerate(stones)}
 
         n = len(stones)
-        @cache
+        memo =[[-1]* (n) for _ in range(n)]
         def dp(i, k):
             if i == n -1:
                 return True
             if i >= n: return False
-            valid = False
-            for jump in range(k-1, k+2):
-                if jump < 1:continue
-                if stones[i] + jump in pos:
-                    valid |= dp(pos[stones[i]+jump], jump)
-            return valid
+            if memo[i][k] == -1:
+                valid = False
+                for jump in range(k-1, k+2):
+                    if jump < 1:continue
+                    if stones[i] + jump in pos:
+                        valid |= dp(pos[stones[i]+jump], jump)
+                memo[i][k] = valid
+            return memo[i][k]
         return dp(0, 0)
 
         
