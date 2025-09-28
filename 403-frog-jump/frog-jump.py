6d533a23eed4252d7c8c4401dd781c1f06187d0a
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-09-28 10:21:18 +0300

    Time: 110 ms (42.38%) | Memory: 19.5 MB (66.03%) - LeetSync

--- 403-frog-jump/frog-jump.py
+++ 403-frog-jump/frog-jump.py
@@ -1,21 +1,21 @@
 class Solution:
     def canCross(self, stones: List[int]) -> bool:
         pos = {num:i for i, num in enumerate(stones)}
-
         n = len(stones)
-        memo =[[-1]* (n) for _ in range(n)]
-        def dp(i, k):
-            if i == n -1:
-                return True
-            if i >= n: return False
-            if memo[i][k] == -1:
-                valid = False
-                for jump in range(k-1, k+2):
-                    if jump < 1:continue
-                    if stones[i] + jump in pos:
-                        valid |= dp(pos[stones[i]+jump], jump)
-                memo[i][k] = valid
-            return memo[i][k]
-        return dp(0, 0)
+        dp = [set() for _ in range(n)]
+        dp[0].add(0)
+        for i in range(n):
+            for k in dp[i]:
+                for jump in [k-1, k, k+1]:
+                    if jump <= 0: continue
+                    nxt_jump = stones[i] + jump
+                    if nxt_jump in pos:
+                        dp[pos[nxt_jump]].add(jump)
+        return len(dp[-1]) > 0
+       
+
+
+
+
 
         
