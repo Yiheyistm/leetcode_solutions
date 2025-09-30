8a9e3544378d1a9a3de2625bfe99ee54521638d1
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-10-01 00:00:18 +0300

    Time: 2502 ms (9.37%) | Memory: 55.2 MB (23.05%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,22 +1,23 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         sum_ = sum(nums)
+        n = len(nums)
         if sum_ % 2: return False
         
-        memo = {}
-        n = len(nums)
+        target = sum_ // 2
+        memo = [[-1] * (target + 1) for _ in range(n + 1)]
         def dp(i, sm):
-            if i == n: 
-                return sm == 0
-            if sm - nums[i] < 0:
-                 return dp(i + 1, sm)
-            if (i, sm) not in memo:
-                for j in range(i, n):
-                    memo[(j, sm)] = dp(j + 1, sm - nums[j])
-                    if memo[(j,sm)]: return True
-            return memo[(i, sm)]
+            if sm == 0:
+                return True
+            if i == n or sm < 0:
+                return False
+            if memo[i][sm] == -1:
+                take = dp(i + 1, sm - nums[i])
+                leave = dp(i + 1, sm)
+                memo[i][sm] = take or leave
+            return memo[i][sm]
         
-        return dp(0, sum_ // 2)
+        return dp(0, target)
 
 
         
