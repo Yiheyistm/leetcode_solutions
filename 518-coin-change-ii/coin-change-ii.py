024d283a94c7f185f652df6eaf1794d36d86e5fe
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-09-30 09:48:05 +0300

    Time: 747 ms (20.60%) | Memory: 50.9 MB (38.22%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,15 +1,17 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        @cache
+        memo = [[-1] * (amount + 1) for _ in range(len(coins) + 1)]
         def dfs(i, rem):
             if rem == 0: return 1
             if i == len(coins): return 0
 
-            leave = dfs(i + 1, rem)
-            take = 0
-            if rem >= coins[i]:
-                take = dfs(i, rem - coins[i])
-            return leave + take
+            if memo[i][rem] == -1:
+                leave = dfs(i + 1, rem)
+                take = 0
+                if rem >= coins[i]:
+                    take = dfs(i, rem - coins[i])
+                memo[i][rem] = leave + take
+            return memo[i][rem]
         return dfs(0, amount)
 
 
