7323ff60fda0d322d72213d5cacbaa07d6a17041
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-09-30 23:00:27 +0300

    Time: 287 ms (49.09%) | Memory: 18.3 MB (53.11%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,13 +1,16 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [[0] * (amount + 1) for _ in range(len(coins) + 1)]
-        for i in range(len(coins) + 1):
-            dp[i][0] = 1
+        dp = [0] * (amount + 1)
+        dp2 = [0] * (amount + 1)
+        dp[0] = 1
+        dp2[0] = 1
 
         for i in range(len(coins) - 1, -1, -1):
             for rem in range(amount + 1):
-                res = dp[i + 1][rem]
+                res = dp2[rem]
                 if rem >= coins[i]:
-                    res += dp[i][rem - coins[i]]
-                dp[i][rem] = res
-        return dp[0][amount]
+                    res += dp[rem - coins[i]]
+                dp[rem] = res
+            dp2 = dp[:]
+            
+        return dp[amount]
