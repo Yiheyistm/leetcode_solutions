6bd185011ffc1e760b520d01d9a1bbaccb04b828
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-10-02 15:06:50 +0300

    Time: 51 ms (97.22%) | Memory: 23.5 MB (79.31%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,9 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         profit = 0
-        effective_buy = prices[0]
+        buy_price = prices[0]
         for p in prices:
-            profit = max(profit, p - effective_buy - fee)
-            effective_buy = min(effective_buy, p - profit)
+            if p - buy_price > fee:
+                profit += p - buy_price - fee
+                buy_price = p - fee
+            buy_price = min(buy_price, p)
         return profit
         
