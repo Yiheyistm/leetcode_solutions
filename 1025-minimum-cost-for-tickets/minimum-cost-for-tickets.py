c01c37f597d7549b94a2930e2fecdd0031f11073
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-10-03 16:26:10 +0300

    Time: 8 ms (23.31%) | Memory: 17.8 MB (82.63%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
@@ -1,5 +1,20 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        dp = [0] * 500
+
+        for day in range(365, -1, -1):
+            d1 = bisect_left(days, day + 1)
+            d7 = bisect_left(days, day + 7)
+            d30 = bisect_left(days, day + 30)
+            
+            dp[day] = min(
+                costs[0] + (dp[days[d1]] if d1 < len(days) else 0),
+                costs[1] + (dp[days[d7]] if d7 < len(days) else 0),
+                costs[2] + (dp[days[d30]] if d30 < len(days) else 0)
+            )
+        
+        return dp[days[0]]
+
         @cache
         def dfs(day):
             if day >= days[-1]:
