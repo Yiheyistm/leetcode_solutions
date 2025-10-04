cf69552ab95a3fb4d37e92fbef2b1c42ec0aa799
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-10-04 18:05:00 +0300

    Time: 7 ms (79.71%) | Memory: 19.6 MB (55.85%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.py
+++ 740-delete-and-earn/delete-and-earn.py
@@ -3,14 +3,14 @@ class Solution:
         cnt = Counter(nums)
         uniques = sorted(cnt.keys())
         N = len(uniques)
-        dp = [0] * (N + 1)
+        prev1, prev2 = 0, 0
         for i in range(N-1, -1, -1):
             earn = uniques[i] * cnt[uniques[i]]
             if  i + 1 < N and (uniques[i+1] - uniques[i] == 1):
-                dp[i] = max(earn + dp[i+2], dp[i+1])
+                prev1, prev2 = max(earn + prev2, prev1), prev1
             else:
-                dp[i] = earn + dp[i + 1]
-        return dp[0]
+                prev1, prev2 = earn + prev1, prev1
+        return prev1
 
            
 
