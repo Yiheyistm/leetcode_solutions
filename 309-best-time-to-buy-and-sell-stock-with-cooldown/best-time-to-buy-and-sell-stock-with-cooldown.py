0e889d24d4490672b111f28447103a0eb46ad542
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-10-05 21:01:57 +0300

    Time: 1 ms (80.83%) | Memory: 18.2 MB (76.43%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,26 +1,25 @@
+# At any given index i there are one of the 3 states:
+# hold: max profit when holding a stock (in hand stock)
+# sold: max profit after selling a stock
+# rest: max profit in cooldown or idle state
+# Rule of state:
+# When you buy, you transition from rest → hold.
+# When you sell, you transition from hold → sold.
+# # After selling, you cool down (sold → rest).
+# State Transition:
+# hold: you can transite to hold state by either keeping holding or buy stock
+# rest: you can transite to rest state by either keeping rest or sell
+# sell: you can transite to sell by selling the stock in hand
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        N = len(prices)
-        # dp = [0] * (N + 2)
-        # for i in range(N-1, -1, -1):
-        #         dp[i] = dp[i + 1] # skip
-        #         for j in range(i+1, N):
-        #             if prices[j] >= prices[i]:
-        #                 dp[i] = max(dp[i], prices[j] - prices[i] + dp[j + 2]) # sell at j that are buy at i
-        # return dp[0]
-        dp = {}
-        def dfs(i, buying):
-            if i >= N: return 0
+        hold = -prices[0] # let's assume you bought a stock and you profit decrease by prices[0]
+        sell = 0 
+        rest = 0
+        for price in prices[1:]: # since you bought at index 0 afte it you can sell it
+            prev_sold = sell
+            sell = hold + price
+            hold = max(hold, rest - price)
+            rest = max(rest, prev_sold)
 
-            if (i, buying) not in dp:
-                cooldown = dfs(i+1, buying)
-                if buying:
-                    buy = dfs(i+1, not buying) - prices[i]
-                    dp[(i, buying)] = max(cooldown, buy)
-        
-                else:
-                    sell = dfs(i+2, not buying) + prices[i]
-                    dp[(i, buying)] = max(cooldown, sell)
-            return dp[(i, buying)]
-        return dfs(0, True)
+        return max(sell, rest) # we know that sell is profitable than hold
         
