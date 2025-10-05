16a6dbd898a3d8e3354a2b92e5a35953b19dc876
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-10-05 20:29:19 +0300

    Time: 7 ms (41.97%) | Memory: 19 MB (29.75%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,15 +1,26 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         N = len(prices)
-        memo = [-1] * N
-        def dfs(i):
-            if i >= N:
-                return 0
-            if memo[i] == -1:
-                memo[i] = dfs(i + 1) # skip
-                for j in range(i+1, N):
-                    if prices[j] >= prices[i]:
-                        memo[i] = max(memo[i], prices[j] - prices[i] + dfs(j + 2)) # sell at j that are buy at i
-            return memo[i]       
-        return dfs(0)
+        # dp = [0] * (N + 2)
+        # for i in range(N-1, -1, -1):
+        #         dp[i] = dp[i + 1] # skip
+        #         for j in range(i+1, N):
+        #             if prices[j] >= prices[i]:
+        #                 dp[i] = max(dp[i], prices[j] - prices[i] + dp[j + 2]) # sell at j that are buy at i
+        # return dp[0]
+        dp = {}
+        def dfs(i, buying):
+            if i >= N: return 0
+
+            if (i, buying) not in dp:
+                cooldown = dfs(i+1, buying)
+                if buying:
+                    buy = dfs(i+1, not buying) - prices[i]
+                    dp[(i, buying)] = max(cooldown, buy)
+        
+                else:
+                    sell = dfs(i+2, not buying) + prices[i]
+                    dp[(i, buying)] = max(cooldown, sell)
+            return dp[(i, buying)]
+        return dfs(0, True)
         
