4d0e0cdeaacf05f0fbae568b86f256227eb8dd9b
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-10-05 15:53:45 +0300

    Time: 54 ms (91.78%) | Memory: 29.3 MB (12.01%) - LeetSync

--- 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.py
+++ 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.py
@@ -1,16 +1,6 @@
 class Solution:
     def longestSubsequence(self, arr: List[int], difference: int) -> int:
-        pos = defaultdict(int)
-        max_seq = 1
+        dp = defaultdict(int)
         for cur_num in reversed(arr):
-            nxt_num = cur_num + difference
-            if nxt_num in pos:
-                pos[cur_num] = max(pos[cur_num], pos[nxt_num] + 1)
-            else: 
-                pos[cur_num] = 1
-            max_seq = max(max_seq, pos[cur_num])
-        return max_seq
-
-
-
-        
+                dp[cur_num] = dp[cur_num + difference] + 1
+        return max(dp.values())
