1358b96813313e8b9e801b5c65d68e52afda3eff
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-10-06 17:24:32 +0300

    Time: 87 ms (42.96%) | Memory: 33.6 MB (27.84%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
@@ -3,7 +3,6 @@ class Solution:
         cnt = defaultdict(int)
         for num in arr:
             cnt[num % k] += 1
-        print(dict(cnt))
         for i in cnt.keys():
             l = k - i
             if i != 0 and (l not in cnt or cnt[l] != cnt[i] or (l == i and cnt[l] % 2 == 1)):
