d7c6cb701065bc2423f76df08cc4c5ca7b96ad17
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-10-07 17:01:12 +0300

    Time: 136 ms (17.02%) | Memory: 17.8 MB (85.53%) - LeetSync

--- 815-champagne-tower/champagne-tower.py
+++ 815-champagne-tower/champagne-tower.py
@@ -1,17 +1,17 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
-        memo = [[-1] * (r +1) for r in range(query_row + 1)]
-        def dfs(r, c):
-            if c < 0 or c > r:
-                return 0
-            if r == 0 and c == 0:
-                return poured
-            if memo[r][c] == -1:
-                left = max((dfs(r - 1, c - 1) - 1) / 2, 0)
-                right = max((dfs(r - 1, c) - 1) / 2, 0)
-                memo[r][c] = left + right
-            return memo[r][c]
-        return min(1, dfs(query_row, query_glass))
+        dp = [0] * 1
+        dp[0]= poured
+        for r in range(1, query_row + 1):
+            dp += [0]
+            for c in range(r,-1,-1):
+                if c == 0:
+                    dp[c] = max((dp[c] - 1) / 2, 0)
+                else:
+                    dp[c] = max((dp[c] - 1) / 2, 0) + max((dp[c - 1] - 1) / 2, 0)
+
+        return min(1, dp[query_glass])
+
      
 
 
