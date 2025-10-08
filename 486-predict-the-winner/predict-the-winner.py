b5a4713b3cb3ff0b4eaaed9441a518ee005bc177
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-10-08 22:51:28 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (45.68%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.py
+++ 486-predict-the-winner/predict-the-winner.py
@@ -1,23 +1,14 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        def option(l, r, isPlayer1):
-            if l > r:
-                return 0
-      
-            if isPlayer1:
-                left_option = nums[l] + option(l + 1, r, False)
-                right_option = nums[r] + option(l, r - 1, False)
-                return max(left_option, right_option)
-            else:
-                option_left = option(l + 1, r, True)
-                option_right = option(l, r - 1, True)
-                return min(option_left, option_right)
-                
-        total = sum(nums)
-        player1 =  option(0, len(nums) -1, True)
-        player2 = total - player1
-        return player1 >= player2
-                    
+        n = len(nums)
+        dp = [[0] * (n) for _ in range(n)]
+        for i in range(n):
+            dp[i][i] = nums[i]
 
-
+        for d in range(1, n):
+            for l in range(n - d):
+                r = l + d
+                dp[l][r] = max(nums[l] - dp[l + 1][r], nums[r] - dp[l][r - 1])
+            
+        return dp[0][n-1] >= 0
         
