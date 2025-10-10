8a3bd5dd4392ceb582abb6fa4fb1ac00c6aeb416
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-10-10 22:34:01 +0300

    Time: 85 ms (89.15%) | Memory: 18.4 MB (45.02%) - LeetSync

--- 1488-sort-integers-by-the-power-value/sort-integers-by-the-power-value.py
+++ 1488-sort-integers-by-the-power-value/sort-integers-by-the-power-value.py
@@ -1,14 +1,31 @@
 class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
-        memo = {}
-        def dfs(n):
-            if n == 1:
-                return 0
-            if n not in memo:
-                memo[n] = 1 + dfs((3 * n) + 1 if n & 1 else n // 2)
-            return memo[n]
+        memo = defaultdict(int)
+        for num in range(lo, hi + 1):
+            n = num
+            path = []
+            while n not in memo and n != 1:
+                path.append(n)
+                n =  3 * n + 1 if n % 2 else n // 2
+                
+            steps = memo[n]
+            for node in reversed(path):
+                steps += 1
+                memo[node] = steps
 
-        ans = list(range(lo, hi+1))
-        ans.sort(key=lambda x: dfs(x))
-        return ans[k-1]
+        arr = [(num, memo[num]) for num in range(lo, hi + 1)]
+        arr.sort(key=lambda x: (x[1], x[0]))
+        return arr[k - 1][0]
+
+
+        # def dfs(n):
+        #     if n == 1:
+        #         return 0
+        #     if n not in memo:
+        #         memo[n] = 1 + dfs((3 * n) + 1 if n & 1 else n // 2)
+        #     return memo[n]
+
+        # ans = list(range(lo, hi+1))
+        # ans.sort(key=lambda x: dfs(x))
+        # return ans[k-1]
         
