88b7050acdfc1de13d1993f0f9fd924aaf081677
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-10-10 11:02:28 +0300

    Time: 1 ms (77.80%) | Memory: 18.9 MB (5.92%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,13 +1,28 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        wd = set(wordDict)
+                    
+        root = {}
+        for word in wordDict:
+            curr = root
+            for ch in word:
+                if ch not in curr:
+                    curr[ch] = {}
+                curr = curr[ch]
+            curr["/"] = 1
+
         memo = [None] * len(s)
         def fn(i):
             if  i == len(s): return True
             if memo[i] == None:
+                node = root
                 memo[i] = False
                 for j in range(i, len(s)):
-                    if s[i:j+1] in wd and fn(j + 1):
+                    ch = s[j]
+                    if ch not in node:
+                        break
+                    node = node[ch]
+
+                    if '/' in node and fn(j + 1):
                         memo[i] = True
                         break
             
