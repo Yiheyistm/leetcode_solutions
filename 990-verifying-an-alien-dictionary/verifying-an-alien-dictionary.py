d325da48c5d22d24dc8ba8578c57157c22d26276
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-10-11 23:08:49 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (93.79%) - LeetSync

--- 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
@@ -3,6 +3,20 @@ class Solution:
         word_order = {}
         for i, ch in enumerate(order):
             word_order[ch] = i
-        sort_words = sorted(words, key=lambda x: [word_order[c] for c in x])
-        return sort_words == words
+        for i in range(len(words) - 1):
+            word1 = words[i]
+            word2 = words[i + 1]
+            for j in range(len(word1)):
+                if j == len(word2):
+                    return False
+                ord1 = word_order[word1[j]]
+                ord2 = word_order[word2[j]]
+
+                if ord1 < ord2:
+                    break
+                elif ord2 < ord1:
+                    return False
+        return True
+
+
         
