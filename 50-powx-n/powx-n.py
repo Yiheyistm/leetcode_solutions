fc48a9327bc28744e1da968dd658d5c75bd9e699
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-10-12 10:57:59 +0300

    Time: 3 ms (6.40%) | Memory: 18.2 MB (11.20%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,19 +1,16 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-       
-        memo = defaultdict(int)
+        memo = {}
         def pow(x, n):
-            if n == 0:
-                return 1
             if n == 1:
                 return x
-            y1 = memo.get(n//2) if n // 2 in memo else pow(x, n//2)
-            y2 = memo.get(n-(n // 2)) if n-(n // 2) in memo else pow(x, n-(n // 2))
-
-            memo[n // 2] = y1
-            memo[n-(n // 2)] = y2
-            return y1 * y2 
-            
-        return pow(x, n) if n > 0 else 1 / (pow(x,-n))
-
+            if n == 0:
+                return 1
+            if n not in memo:
+                half_1 = pow(x, n // 2)
+                half_2 = pow(x, n - (n // 2))
+                memo[n] =  half_1 * half_2
+            return memo[n]
+        return pow(x, n) if n > 0 else 1 / pow(x, -n)
+        
         
