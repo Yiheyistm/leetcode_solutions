41b03e97f54daf6170eb27e5f5fdd31a415ed4d1
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-10-12 16:50:50 +0300

    Time: 247 ms (55.95%) | Memory: 29 MB (7.74%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,22 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(row, col):
-            if row >= rows or row < 0 or col >= cols or col < 0 or grid[row][col] == '0':
-                return
-            grid[row][col] = '0'
-            dfs(row + 1, col)
-            dfs(row - 1, col)
-            dfs(row, col + 1)
-            dfs(row, col - 1)
-        
-        cnt = 0
-        rows = len(grid)
-        cols = len(grid[0])
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == '1':
-                    dfs(i, j)
-                    cnt += 1
-        return cnt
+        R, C = len(grid), len(grid[0])
+        visited = set()
+        def dfs(i, j):
+            if (i < 0 or i >= R or j < 0 or j >= C) or grid[i][j] == "0" or (i, j) in visited:
+                return 0
+            visited.add((i, j))
+            ild = 1
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j - 1)
+            dfs(i, j + 1)
+            return ild
+
+        ans = 0
+        for i in range(R):
+            for j in range(C):
+                if (i, j) not in visited and grid[i][j] == "1":
+                    ans += dfs(i, j)
+        return ans
+
+
+
 
         
