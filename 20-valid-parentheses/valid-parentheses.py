3eaa2dc3531d9bb60a4b75522f190fc977fcba6b
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-10-12 10:29:11 +0300

    Time: 3 ms (32.54%) | Memory: 17.8 MB (56.74%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,16 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        N = len(s)
         matched_parentheses = {"(": ")", "[": "]", "{": "}"}
         stack = []
-        for p in s:
+        for i in range(N):
+            p = s[i]
             if p in "([{":
                 stack.append(p)
-            elif p in ")]}" and stack:
-                if matched_parentheses[stack[-1]] != p:
-                    return False
+            elif stack and matched_parentheses[stack[-1]] == p:
                 stack.pop()
             else:
                 return False
+
+            if len(stack) > (N - i - 1):
+                return False
+            
 
         return True if not stack else False
         
