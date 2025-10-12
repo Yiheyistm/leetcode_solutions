68f70400d494e749c279e1f8267c04e469bc7540
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-10-12 17:01:41 +0300

    Time: 237 ms (76.74%) | Memory: 20 MB (82.23%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,23 +1,19 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         R, C = len(grid), len(grid[0])
-        visited = set()
-        def dfs(i, j):
-            if (i < 0 or i >= R or j < 0 or j >= C) or grid[i][j] == "0" or (i, j) in visited:
-                return 0
-            visited.add((i, j))
-            ild = 1
-            dfs(i + 1, j)
-            dfs(i - 1, j)
-            dfs(i, j - 1)
-            dfs(i, j + 1)
-            return ild
-
         ans = 0
         for i in range(R):
             for j in range(C):
-                if (i, j) not in visited and grid[i][j] == "1":
-                    ans += dfs(i, j)
+                if grid[i][j] == "1":
+                    ans += 1
+                    path = deque([(i, j)])
+                    while path:
+                        r, c = path.popleft()
+                        for di, dj in [(r + 1, c), (r - 1, c),(r, c - 1),(r, c + 1)]:
+                            if (di < 0 or di >= R or dj < 0 or dj >= C) or grid[di][dj] == "0":
+                                continue
+                            path.append((di, dj))
+                            grid[di][dj] = "0"
         return ans
 
 
