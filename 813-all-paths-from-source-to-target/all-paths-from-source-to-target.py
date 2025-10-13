2bea40e3739517695dab24fff17671044cb5cb41
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-10-13 23:16:52 +0300

    Time: 7 ms (40.46%) | Memory: 19 MB (81.01%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -2,18 +2,37 @@ class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         target = len(graph) - 1
         res = []
-        def dfs(node, store):
+        queue = deque([[0]])
+        
+        while queue:
+            current_path = queue.popleft()
+            node = current_path[-1]
+            
             if node == target:
-                res.append(store[:])
-                return
+                res.append(current_path)
+                continue 
 
             for neigh in graph[node]:
-                store.append(neigh)
-                dfs(neigh, store)
-                store.pop()
-            return
-
-        dfs(0, [0])
+                new_path = list(current_path)
+                new_path.append(neigh)
+                queue.append(new_path)
+                                
         return res
+
+        # target = len(graph) - 1
+        # res = []
+        # def dfs(node, store):
+        #     if node == target:
+        #         res.append(store[:])
+        #         return
+
+        #     for neigh in graph[node]:
+        #         store.append(neigh)
+        #         dfs(neigh, store)
+        #         store.pop()
+        #     return
+
+        # dfs(0, [0])
+        # return res
 
         
