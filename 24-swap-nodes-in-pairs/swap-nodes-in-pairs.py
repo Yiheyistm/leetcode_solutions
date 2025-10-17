c50ae21e8c10eb3ec797541f9fc1e3e1c0086c98
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-10-17 09:56:26 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (53.76%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.py
@@ -7,18 +7,19 @@ class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
             return head
-
-        cur = head
-        dummy = ListNode()
-        p = dummy
-        while cur and cur.next:
-            p.next = cur.next
-            cur.next = p.next.next
-            p.next.next = cur
-            p = p.next.next
-            cur = cur.next
-        return dummy.next
-
-
+        isSwaped = False
+        curr = head
+        while curr:
+            if not isSwaped and curr.next:
+                curr_val = curr.val
+                nxt_val = curr.next.val
+                curr.val = nxt_val
+                curr.next.val = curr_val
+                curr = curr.next
+                isSwaped = True
+            else:
+                curr = curr.next
+                isSwaped = False
+        return head
 
         
