88cb8616dbb697ccea554fdd9bd1dcee1547c72a
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-10-30 15:37:44 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (91.46%) - LeetSync

--- 86-partition-list/partition-list.py
+++ 86-partition-list/partition-list.py
@@ -1,33 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-        if not head or not head.next: return head
+        if not head:
+            return head
+        less = ListNode()
+        greater = ListNode()
+        ls = less
+        gr = greater
+        while head:
+            if head.val >= x:
+                gr.next = head
+                gr = gr.next
+            else:
+                ls.next = head
+                ls = ls.next
+            head = head.next
+        gr.next = None
+        ls.next = greater.next
+        return less.next
 
-        min_head = max_head = None
-        min_temp = max_temp = None
-
-        curr = head
-        while curr:
-            currVal = curr.val
-            if currVal < x:
-                if min_head:
-                    min_temp.next = curr
-                    min_temp = min_temp.next
-                else:
-                    min_head = curr
-                    min_temp = curr
-                if not curr.next and max_temp: max_temp.next = None
-            else :
-                if max_head:
-                    max_temp.next = curr
-                    max_temp = max_temp.next
-                else:
-                    max_head = curr
-                    max_temp = curr
-                if not curr.next and min_temp: min_temp.next = None
-                
-            curr = curr.next
-        if min_temp:
-            min_temp.next = max_head
-        else: return max_head
-
-        return min_head
+        
