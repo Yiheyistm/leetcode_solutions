3d10ab9b5b462bccbb28c34092b65caf72fda842
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-11-02 23:18:35 +0300

    Time: 59 ms (9.34%) | Memory: 17.9 MB (42.81%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
@@ -1,9 +1,15 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
         ans = 0
-        for i in range(1, len(nums) + 1):
-            ls = combinations(nums, i)
-            for l in ls:
-                ans += reduce(xor, l)
+        for i in range(2**len(nums)):
+            k = 0
+            xor = 0
+            while i > 0:
+                if i & 1:
+                    xor ^= nums[k]
+                i >>= 1
+                k += 1
+            ans += xor
         return ans
+
         
