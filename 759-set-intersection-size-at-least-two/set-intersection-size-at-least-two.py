4a4b3579a8331add89a62365f32bebf8b744fe60
Yiheyis Tamir <130172036+Yiheyistm@users.noreply.github.com>
2025-11-21 14:56:46 +0300

    Time: 12 ms (42.76%) | Memory: 19 MB (79.31%) - LeetSync

--- 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.py
+++ 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.py
@@ -1,42 +1,18 @@
 class Solution:
     def intersectionSizeTwo(self, intervals: List[List[int]]) -> int:
-        n=len(intervals)
-        intervals.sort(key=lambda x:x[1])
-        prev1=intervals[0][1]-1
-        prev2=intervals[0][1]
-        c=2
-        for i in range(1,n):
-            if prev2<intervals[i][0]:
-                prev1=intervals[i][1]-1
-                prev2=intervals[i][1]
-                c+=2
-            elif prev1<intervals[i][0]:
-                if intervals[i][1]==prev2:
-                    prev1=intervals[i][1]-1
-                else:
-                    prev1=intervals[i][1]
-                prev1,prev2=min(prev1,prev2),max(prev1,prev2)
-                c+=1
-        return c
-        
-        # intervals.sort(key=lambda item: (item[1], item[0]))
-        # st = set()
-        # ans = SortedSet([-inf])
-        # for i in range(len(intervals)):
-        #     if i in st: continue
-        #     s, e = intervals[i]
-        #     st.add(i)
-        #     for j in range(i+1, len(intervals)):
-        #         s1, e1  = intervals[j]
-        #         if s1 < e:
-        #             s = max(s, s1)
-        #         elif s1 == e and not(s1 < ans[-1] < e1):
-        #             ans.add(e1)
-        #         elif s1 > e:
-        #             break
-        #         st.add(j)
-        #     ans.add(s)
-        #     ans.add(e)
-        #     print(ans, st)
-        # return len(ans) - 1
+        intervals.sort(key=lambda item: (item[1], -item[0]))
+        p1, p2 = intervals[0][1]-1, intervals[0][1]
+        count = 2
+        for i in range(1, len(intervals)):
+            s, e = intervals[i]
+            inside = (p1 >= s) + (p2 >= s)
+            if inside == 2: continue
+            elif inside == 1:
+                count += 1
+                p2, p1 = e, p2
+            elif s > p2:
+                count += 2
+                p1 = e-1
+                p2 = e
+        return count
         
