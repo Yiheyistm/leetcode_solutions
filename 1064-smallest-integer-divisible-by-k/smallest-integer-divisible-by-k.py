d3a1ab18cbb34034a661ee227021dc2f459601f6
Yiheyis Tamir <yiheyisyt23@gmail.com>
2025-11-25 20:08:22 +0300

    Time: 967 ms (21.45%) | Memory: 17.8 MB (56.41%) - LeetSync

--- 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.py
+++ 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.py
@@ -3,17 +3,12 @@ class Solution:
         if k % 2 == 0 or k % 5 == 0:
             return -1
         
-        st = set()
-        start = 0
-        i = 0
-        while len(st) <= k:
+        start = 1
+        i = 1
+        while start % k and i <= k:
             i += 1
             start = start * 10 + 1
-            rem = start % k
-            if not rem:
-                return i
-            st.add(rem)
-        return -1
+        return i
 
 
         
